16bb9f08a7ae3e6ab9a93859500d11df54aba27b
Diy-101 <thehorsediy101@gmail.com>
2024-04-27 00:25:15 +0300

    add button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import {useEffect} from "react";
+const tg = window.Telegram.WebApp
 
 function App() {
+    const onClose = () => {
+        tg.close();
+    }
+
+    useEffect(() => {
+        tg.ready();
+    }, []);
+
   return (
     <div className="App">
-
+      <button onClick={onClose}>Close</button>
     </div>
   );
 }
